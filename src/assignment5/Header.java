5022121edc5441dcf0f27c84856616d98491d9fa
MichaelFortanely <60264854+MichaelFortanely@users.noreply.github.com>
2021-07-15 14:05:50 -0500

    Initial commit

---
+++ src/assignment5/Header.java
@@ -0,0 +1,18 @@
+/* CRITTERS GUI <MyClass.java>
+ * EE422C Project 5 submission by
+ * Replace <...> with your actual data.
+ * <Student1 Michael Fortanely>
+ * <Student1 mjf2894>
+ * <Student1 76000>
+ * <Student2 Randunu Thrikawala>
+ * <Student2 ryt238>
+ * <Student2 76000>
+ * Slip days used: <0>
+ * Summer 2021
+ */
+
+/*
+   Describe here known bugs or issues in this file. If your issue spans multiple
+   files, or you are not sure about details, add comments to the README.txt file.
+ */
+package assignment5;
